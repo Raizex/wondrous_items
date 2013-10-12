a7b0274cd880a3d00be821478c302f3cc01fece9
Justin Huffman <jwhuffman@gmail.com>
2013-10-12 01:13:34 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WounderousItems::Application.config.secret_key_base = 'dd967e92a08bac938f53e61fedfbbc2ec57e86ee54bc8b5ea752033ee606eeebea9041631ab2e8a19b9552951e9ca802955590d4f262930c258a265eb15db4c0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WounderousItems::Application.config.session_store :cookie_store, key: '_wounderous_items_session'
